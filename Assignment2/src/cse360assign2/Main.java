17bb41186bf63e7f9c556fef5cd304f8c162e35d
oscar veloria <oveloria@asu.edu>
2019-10-07 23:55:36 -0700

    First change, changed start.java to Main.java for ease of use

--- Assignment2/src/cse360assign2/Main.java
+++ Assignment2/src/cse360assign2/Main.java
@@ -1,8 +1,12 @@
 package cse360assign2;
 
-public class start {
+public class Main {
 	public static void main(String [] args) {
 		System.out.println("hello");
+		
+		
+		System.out.println("This is test 21");
 	}
+	
 	
 }
